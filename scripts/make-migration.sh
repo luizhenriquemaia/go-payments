33b05f54f1b0c95785157a04ab23f102d88538c8
Luiz Henrique <luizhenriquembh@gmail.com>
2024-05-24 17:53:47 -0300

    feat[scripts]: add script to make migrations

---
+++ scripts/make-migration.sh
@@ -0,0 +1,24 @@
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -n complementary name of file"
+   echo -e "\t-n complementary name of file"
+   exit 1 # Exit script after printing help
+}
+
+while getopts ":n:?:" opt
+do
+    case "$opt" in
+        n ) file_name=$(echo "$OPTARG" | sed -e 's/ /_/g');;
+        ? ) helpFunction ;;
+    esac
+done
+
+if [ -z "$file_name" ] || [ "$file_name" == " " ];
+then
+    echo -e "\nError: file name is required";
+    helpFunction
+fi
+
+goose -dir "configs/db/migrations" create $file_name sql
+exit 1
