7ddec4bfa4292eb37d520f21cdcc20e28b713a81
Luiz Henrique <luizhenriquembh@gmail.com>
2024-08-04 19:38:34 -0300

    test[expenses]: add tests for list expenses route

--- internal/expenses/tests/route_test.go
+++ internal/expenses/tests/route_test.go
@@ -1,14 +1,24 @@
 package tests
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	test_utils "go-payments/tests"
 
+	"go-payments/internal/expenses/api"
+	"go-payments/internal/expenses/entities"
+
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/suite"
 )
+
+type ListResponse struct {
+	Data []entities.ExpenseEntityResponse
+}
 
 type TestListExpensesSuite struct {
 	suite.Suite
@@ -16,20 +26,41 @@ type TestListExpensesSuite struct {
 
 func TestExpensesApisSuite(test *testing.T) {
 	suite.Run(test, &TestListExpensesSuite{})
-	result := AddTestExpenses(3)
-	log.Printf("added expenses %+v", result)
 }
 
 func (suite *TestListExpensesSuite) SetupSuite() {
 	test_utils.InitTestDb()
+	AddTestExpenses(3)
+}
+
+func (suite *TestListExpensesSuite) SetupRouter() *gin.Engine {
+	r := gin.Default()
+	router := r.Group("api/v1/")
+	{
+		api.ExpensesRoutes(router)
+	}
+	return r
+}
+
+func (suite *TestListExpensesSuite) SendRequest() *httptest.ResponseRecorder {
+	router := suite.SetupRouter()
+	resp_writer := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/api/v1/expense", nil)
+	router.ServeHTTP(resp_writer, req)
+	return resp_writer
 }
 
 func (suite *TestListExpensesSuite) TestListExpenses() {
-	resp, err := http.NewRequest("GET", "/", nil)
+	resp := suite.SendRequest()
+	var resp_body ListResponse
+	err := json.Unmarshal(resp.Body.Bytes(), &resp_body)
 	if err != nil {
-		log.Printf("request error %v", err)
+		log.Fatalf("Error on unmarshal response %v", err)
 	}
-	log.Printf("response %+v", resp)
-
-	suite.Equal(1, 2)
+	log.Printf("resp body %+v", resp_body)
+	suite.Equal(200, resp.Result().StatusCode)
+	suite.Equal(3, len(resp_body.Data))
+	suite.Equal(int64(3), resp_body.Data[0].Id)
+	suite.Equal(int64(2), resp_body.Data[1].Id)
+	suite.Equal(int64(1), resp_body.Data[2].Id)
 }
